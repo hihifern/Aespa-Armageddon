b91135ea01ce2c5ae07cd69971875c8b65b91367
hihifern <chaya13ju@gmail.com>
2024-09-03 21:45:28 +0700

    footer

--- js/script.js
+++ js/script.js
@@ -1,44 +1,69 @@
-// music-mv
-
 const armageddon = document.getElementById("armageddon");
 const supernova = document.getElementById("supernova");
 const mainImage = document.getElementById("main-image");
-const link = document.querySelector(".mv a")
+const link = document.querySelector(".mv a");
 
 let lastImage = mainImage.src;
 
 function changeImage(src, url) {
-    mainImage.classList.add('hidden'); // เพิ่มคลาสเพื่อทำให้รูปภาพโปร่งใส
+    mainImage.classList.add('hidden');
     setTimeout(() => {
-        mainImage.src = src; // เปลี่ยน src ของรูปภาพ
-        link.href = url; // เปลี่ยนลิงก์
-        mainImage.classList.remove('hidden'); // ลบคลาสเพื่อทำให้รูปภาพปรากฏขึ้น
-    }, 500); // เวลาในการรอเพื่อให้การเปลี่ยนภาพเป็นไปอย่างช้า (500ms)
+        mainImage.src = src;
+        link.href = url;
+    }, 400);
+
+    setTimeout(() => {
+        mainImage.classList.remove('hidden');
+    }, 500);
 }
 
+
 armageddon.addEventListener("mouseenter", () => {
-    lastImage = mainImage.src;
-    mainImage.src = "img/mv-armageddon.jpg";
-    link.href = "https://youtu.be/nFYwcndNuOY?si=SuyzeJ-gsnOaR8OE";
+    changeImage("img/mv-armageddon.jpg", "https://youtu.be/nFYwcndNuOY?si=SuyzeJ-gsnOaR8OE");
     armageddon.style.fontWeight = "bold";
 });
 
-
 supernova.addEventListener("mouseenter", () => {
-    lastImage = mainImage.src;
-    mainImage.src = "img/mv-supernova.jpg";
-    link.href = "https://youtu.be/phuiiNCxRMg?si=rAWF9poejOaIVfVX"
+    changeImage("img/mv-supernova.jpg", "https://youtu.be/phuiiNCxRMg?si=rAWF9poejOaIVfVX");
     supernova.style.fontWeight = "bold";
 });
 
 
 armageddon.addEventListener("mouseleave", () => {
-
     armageddon.style.fontWeight = "normal";
 });
 
 supernova.addEventListener("mouseleave", () => {
-
     supernova.style.fontWeight = "normal";
 });
 
+
+//รูปmembers
+let currentIndex = 0;
+const slides = document.querySelector('.member-slide');
+const totalSlides = document.querySelectorAll('.member').length;
+const menuItems = document.querySelectorAll('.mem-text li');
+
+function showSlide(index) {
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+    highlightMenu(currentIndex);
+}
+
+function highlightMenu(index) {
+    menuItems.forEach(item => item.classList.remove('selected'));
+    menuItems[index].classList.add('selected'); 
+}
+
+// เพิ่ม event listeners สำหรับแต่ละปุ่มในรายการ
+menuItems.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        currentIndex = index;
+        showSlide(currentIndex);
+        highlightMenu(currentIndex);
+    });
+});
